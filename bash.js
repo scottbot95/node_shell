65a77c1912d199cc35789670edbfdd3a6416723e
Scott Techau <scotttechau@gmail.com>
2018-11-06 11:56:25 -0800

    moved pwd to it's own module

--- bash.js
+++ bash.js
@@ -1,15 +1,14 @@
+const pwd = require('./pwd');
+
 //Output a prompt
-process.stdout.write('prompt> ');
+const prompt = 'prompt> ';
+
+process.stdout.write(prompt);
 //The stdin 'data' event fires after a user tyes in a line
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim(); //remove the newline
-    let output;
     if (cmd.startsWith('pwd')) {
-        output = pwd();
+        pwd();
     }
-    if (output) { process.stdout.write(output) }
-    process.stdout.write('\nprompt > ')
 });
-function pwd() {
-    return process.cwd()
-}
+
